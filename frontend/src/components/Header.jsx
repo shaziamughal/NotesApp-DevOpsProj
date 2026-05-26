e63e2c874f180f59a352dae90143ef24225e5824
shaziamughal <bscs22f09@namal.edu.pk>
2026-05-27 02:48:23 +0500

    Final CICD Check

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -17,7 +17,7 @@ function Header() {
   return (
     <header className='header'>
       <div className='logo'>
-        <Link to='/'>GoalSetter</Link>
+        <Link to='/'>GoalSetter App </Link>
       </div>
       <ul>
         {user ? (
