f0452503b332590c2e003e847a5a1c8a312eaf6e
shaziamughal <bscs22f09@namal.edu.pk>
2026-05-27 02:38:04 +0500

    Fixed frontend backend API URL

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
-import { apiUrl } from '../../app/apiUrl'
 
-//const API_URL = 'http://localhost:5000/api/goals/'
-//const API_URL = apiUrl('/api/goals')
-const API_URL = '/api/goals/'
+const API_URL =
+  'https://notes-backend-app.azurewebsites.net/api/goals/'
+
 // Create new goal
 const createGoal = async (goalData, token) => {
   const config = {
@@ -12,7 +11,7 @@ const createGoal = async (goalData, token) => {
     },
   }
 
-  const response = await axios.post(`${API_URL}/`, goalData, config)
+  const response = await axios.post(API_URL, goalData, config)
 
   return response.data
 }
@@ -25,7 +24,7 @@ const getGoals = async (token) => {
     },
   }
 
-  const response = await axios.get(`${API_URL}/`, config)
+  const response = await axios.get(API_URL, config)
 
   return response.data
 }
@@ -38,20 +37,7 @@ const deleteGoal = async (goalId, token) => {
     },
   }
 
-  const response = await axios.delete(`${API_URL}/${goalId}`, config)
-
-  return response.data
-}
-
-// Update user goal
-const updateGoal = async (goalId, goalData, token) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  }
-
-  const response = await axios.put(`${API_URL}/${goalId}`, goalData, config)
+  const response = await axios.delete(API_URL + goalId, config)
 
   return response.data
 }
@@ -60,7 +46,6 @@ const goalService = {
   createGoal,
   getGoals,
   deleteGoal,
-  updateGoal,
 }
 
 export default goalService
